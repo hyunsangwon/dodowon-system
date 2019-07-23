d699963c05860529328068cbc5243a8f90049de6
HyunSangWon <hyunsangwon93@gmail.com>
2019-07-23 15:20:08 +0900

    중간점검

--- src/main/java/com/dodo/system/interceptor/Interceptor.java
+++ src/main/java/com/dodo/system/interceptor/Interceptor.java
@@ -2,6 +2,7 @@ package com.dodo.system.interceptor;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,12 +17,16 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dodo.system.domain.EmpPrincipal;
+import com.dodo.system.mapper.DocsMapper;
 import com.dodo.system.service.EmpService;
 
 @Component
 public class Interceptor implements HandlerInterceptor {
 
 	private static final Logger logger = LoggerFactory.getLogger(Interceptor.class);
+	
+	@Autowired
+	private DocsMapper docsMapper;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -43,13 +48,26 @@ public class Interceptor implements HandlerInterceptor {
 			ModelAndView modelAndView) throws Exception {
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		EmpPrincipal empPrincipal = (EmpPrincipal) auth.getPrincipal();		
-		modelAndView.addObject("roleName", empPrincipal.getRoleVO().getRole_name());
-		modelAndView.addObject("empNo",empPrincipal.getEmpNo());
+		EmpPrincipal empPrincipal = (EmpPrincipal) auth.getPrincipal();				
+		String roleName = empPrincipal.getRoleVO().getRole_name();
+		int empNo = empPrincipal.getEmpNo();
+		
+		modelAndView.addObject("roleName",roleName);
+		modelAndView.addObject("empNo",empNo);
 		
 		SimpleDateFormat format = new SimpleDateFormat ("HH:mm");
 		Calendar time = Calendar.getInstance();
 		modelAndView.addObject("now",format.format(time.getTime()));
+		
+		if(roleName.equals("USER")) {
+			List<Integer> listCnt = docsMapper.totalReportingCnt(empNo,"i","reporting");
+			int totalCnt = 0;	
+			for(int x=0; x< listCnt.size(); x++) {
+				totalCnt += listCnt.get(x);
+			}
+			/* 결재해야될 문서 알람 개수 */
+			modelAndView.addObject("msgCnt",totalCnt);
+		}
 		
 		logger.debug("======================================================");
 	}
