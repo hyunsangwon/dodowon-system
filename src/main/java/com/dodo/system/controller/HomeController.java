218917e82435efc01e7ba0a7833c774736022814
HyunSangWon <hyunsangwon93@gmail.com>
2019-07-23 16:10:20 +0900

    중간점검

--- src/main/java/com/dodo/system/controller/HomeController.java
+++ src/main/java/com/dodo/system/controller/HomeController.java
@@ -1,27 +1,22 @@
 package com.dodo.system.controller;
 
-import com.dodo.system.domain.EmpPrincipal;
-import com.dodo.system.service.DocsService;
-import com.dodo.system.service.HomeService;
-import com.dodo.system.vo.EmpVO;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dodo.system.service.DocsService;
 
 /**
  * Author Sangwon Hyun on 2019-07-07
  */
 @Controller
-public class HomeController {
+public class HomeController implements ErrorController{
     /* global setting login,error,log ...*/
 
     @Autowired
@@ -43,19 +38,27 @@ public class HomeController {
         return "redirect:/home/docs/holiday/i/1";
     }
 
-    /*작업중*/
-    @GetMapping("/error")
-    public String loadErrorPage(ModelMap model,HttpServletRequest request) throws Exception{
+    @RequestMapping("/error")
+    public String handleError(ModelMap model,HttpServletRequest request) {
 
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if(status.equals("404")){
-            model.addAttribute("msg","페이지를 찾을 수 없습니다.");
-        }
-
-        model.addAttribute("errorCode",status.toString());
-
-        return "error/error";
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		String errorNumber = status.toString();
+		
+		switch(errorNumber) {
+			case "404" : model.addAttribute("msg","404"); return "error/error";
+			case "500" : model.addAttribute("msg","500"); return "error/error";
+			case "403" : model.addAttribute("msg","403"); return "error/error";
+			case "400" : model.addAttribute("msg","400"); return "error/error";
+			default : model.addAttribute("msg","default");return "error/error";
+		}
+		
     }
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return "/error";
+	}
+    
 
 }
