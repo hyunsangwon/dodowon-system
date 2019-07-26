e4812f97e87559dfd633159ab32ba702360d006d
HyunSangWon <hyunsangwon93@gmail.com>
2019-07-26 11:55:16 +0900

    active jqeury add

--- src/main/java/com/dodo/system/api/RootQuery.java
+++ src/main/java/com/dodo/system/api/RootQuery.java
@@ -1,12 +1,13 @@
 package com.dodo.system.api;
 
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-import com.dodo.system.service.EmpService;
-import com.dodo.system.vo.EmpVO;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.dodo.system.service.EmpService;
+import com.dodo.system.vo.EmpVO;
 
 /**
  * Author Sangwon Hyun on 2019-07-25
